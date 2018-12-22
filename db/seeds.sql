5db739e1da3cd982c5ac216a299883dd5bcb38df
jason villaluz <jasonvillaluz@jasons-mbp.lan>
2018-12-21 20:09:31 -0800

    first set of files

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+USE burgers_db;
+INSERT INTO burgers (burger_name) VALUES ('Taco Burger');
+INSERT INTO burgers (burger_name) VALUES ('Onion Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Frito Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Trout Burger ', true);
+
